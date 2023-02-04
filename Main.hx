e4a97140b306926789923bc359e3bb7fa2434e09
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-02-04 00:19:50 -0600

    level design plus rendering it

--- Main.hx
+++ Main.hx
@@ -1,37 +1,57 @@
 class Main extends hxd.App {
+    public var ldtkProject:LdtkProject;
+
+    public static inline final VIEW_WIDTH = 960;
+    public static inline final VIEW_HEIGHT = 450;
+
     public static function main() {
         new Main();
     }
 
     override function init() {
         super.init();
-        initRects();
+        loadLevel();
+    }
 
+    function initScene() {
+        //sets the scene to a fixed size with space around it to fill the window. Hardcoding this for now, should eventually configurable via game UI
+       s2d.scaleMode = LetterBox(VIEW_WIDTH, VIEW_HEIGHT, true, Center, Center);
     }
 
     override function update(dt:Float) {
         super.update(dt);
     }
 
-    function initRects() {
-        for (i in 0...100) {
-            var rectGraphic = new h2d.Graphics();
-            var leftX = Std.int(Math.random()*1000);
-            var topY = Std.int(Math.random()*1000);
-            var width = Std.int(Math.random()*100);
-            var height = Std.int(Math.random()*100);
-            // rectGraphic.moveTo(leftX, topY);
-            rectGraphic.lineStyle(3, 0xff0000, 1);
-            rectGraphic.beginFill(0x770000, 1);
-            rectGraphic.drawRect(leftX, topY, width, height);
-            rectGraphic.endFill();
-            s2d.addChild(rectGraphic);
-            // var tile = h2d.Tile.fromColor(0x770000, Std.int(Math.random()*50), 
-            //     Std.int(Math.random()*50));
-            // var bitmap = new h2d.Bitmap(tile);
-            // bitmap.setPosition(Std.int(Math.random()*1000), 
-            //     Std.int(Math.random()*1000));
-            // s2d.addChild(bitmap);
-        }
+    function loadLevel() {
+        ldtkProject = new LdtkProject();
+        var level0 = ldtkProject.all_levels.Level_0.l_Floor.render();
+        level0.setPosition(ldtkProject.all_levels.Level_0.worldX, ldtkProject.all_levels.Level_0.worldY);
+        s2d.addChild(level0);
+        var level1 = ldtkProject.all_levels.Level_1.l_Floor.render();
+        level1.setPosition(ldtkProject.all_levels.Level_1.worldX, ldtkProject.all_levels.Level_1.worldY);
+        s2d.addChild(level1);
+        var level2 = ldtkProject.all_levels.Level_2.l_Floor.render();
+        level2.setPosition(ldtkProject.all_levels.Level_2.worldX, ldtkProject.all_levels.Level_2.worldY);
+        s2d.addChild(level2);
+        var level3 = ldtkProject.all_levels.Level_3.l_Floor.render();
+        level3.setPosition(ldtkProject.all_levels.Level_3.worldX, ldtkProject.all_levels.Level_3.worldY);
+        s2d.addChild(level3);
+        var level4 = ldtkProject.all_levels.Level_4.l_Floor.render();
+        level4.setPosition(ldtkProject.all_levels.Level_4.worldX, ldtkProject.all_levels.Level_4.worldY);
+        s2d.addChild(level4);
+        var level5 = ldtkProject.all_levels.Level_5.l_Floor.render();
+        level5.setPosition(ldtkProject.all_levels.Level_5.worldX, ldtkProject.all_levels.Level_5.worldY);
+        s2d.addChild(level5);
     }
+
+    override function loadAssets(onLoaded:() -> Void) {
+        #if js
+        hxd.Res.initEmbed();
+        #else
+        hxd.res.Resource.LIVE_UPDATE = true;
+        hxd.Res.initLocal();
+        #end
+        super.loadAssets(onLoaded);
+    }
+
 }
---
+++ src/LdtkProject.hx
@@ -0,0 +1,2 @@
+private typedef _Tmp =
+    haxe.macro.MacroType<[ ldtk.Project.build("src/res/ldtk/entbros.ldtk") ]>;
