2d0af25539c4449273182bf887596d03b52b2bba
mrrogge <5900418+mrrogge@users.noreply.github.com>
2023-02-05 11:28:48 -0600

    make all tiles have no offset, aabb offset logic isn't working

--- src/Tiles.hx
+++ src/Tiles.hx
@@ -6,8 +6,8 @@ class Tiles {
     public final blueButton:Array<h2d.Tile>;
 
     public function new() {
-        ent = makeTileArrayFromFixedSize(hxd.Res.img.ent1_png, 0, 0, 20, 36, 1, 10, 18);
-        tree = makeTileArrayFromFixedSize(hxd.Res.img.tree_png, 0, 0, 64, 64, 1, 32, 32);
+        ent = makeTileArrayFromFixedSize(hxd.Res.img.ent1_png, 0, 0, 20, 36, 1, 0, 0);
+        tree = makeTileArrayFromFixedSize(hxd.Res.img.tree_png, 0, 0, 64, 64, 1, 0, 0);
         greenButton = makeTileArrayFromFixedSize(hxd.Res.img.greenButton_png, 0, 0, 32, 32, 2, 0, 0);
         redButton = makeTileArrayFromFixedSize(hxd.Res.img.redButton_png, 0, 0, 32, 32, 2, 0, 0);
         blueButton = makeTileArrayFromFixedSize(hxd.Res.img.blueButton_png, 0, 0, 32, 32, 2, 0, 0);
